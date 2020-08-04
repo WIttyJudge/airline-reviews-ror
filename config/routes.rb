09fa98a2a20e436ab05f5f581bdec917cbe7657c
Ruslan <kros8925@gmail.com>
2020-08-04 17:42:57 +0300

    Add routes for graphql and resource airlines

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api/v2/graphql"
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+    end
+
+    namespace :v2 do
+      match "graphql", to: "graphql#execute", via: %i[get post delete]
+    end
+  end
+
 end
