a7bd81e3a10134198380a2c900a133677338eb05
Ruslan <kros8925@gmail.com>
2020-08-04 16:46:58 +0300

    Add testing for airline model

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -20,15 +20,24 @@ RSpec.describe Airline, type: :model do
     it "name is required" do
       @airline.name = ""
       expect(@airline.valid?).to be(false)
+      expect(@airline.errors.messages[:name]).to eq ["can't be blank"]
     end
 
     it "image_url is required" do
       @airline.image_url = ""
       expect(@airline.valid?).to be(false)
+      expect(@airline.errors.messages[:image_url]).to eq ["can't be blank"]
     end
     
     it "all records are not required" do
       expect(@airline.valid?).to be(true)
+    end
+
+    context "before_create" do
+      it "name_to_title" do
+        airline = Airline.create(:name => "test:test TesT", :image_url => "https://test.com")
+        expect(airline.name).to eq("Test:Test Tes T")
+      end
     end
 
   end
