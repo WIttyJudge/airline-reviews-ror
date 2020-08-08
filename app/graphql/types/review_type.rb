d0a203b9f59b5d77c8ccd8272a92e7b5179e4db5
Ruslan <kros8925@gmail.com>
2020-08-08 22:27:45 +0300

    Sets null to false, because fields cannot be nil.

--- app/graphql/types/review_type.rb
+++ app/graphql/types/review_type.rb
@@ -1,12 +1,12 @@
 module Types
   class ReviewType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
-    field :description, String, null: true
-    field :score, Integer, null: true
+    field :title, String, null: false
+    field :description, String, null: false
+    field :score, Integer, null: false
     # field :airline_id, Integer, null: true
     field :airline, AirlineType, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
