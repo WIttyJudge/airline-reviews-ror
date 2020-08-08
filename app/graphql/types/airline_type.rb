96620e7d261b0483fe428d31353b833c98cef977
Ruslan <kros8925@gmail.com>
2020-08-08 22:24:25 +0300

    Add average_score field

--- app/graphql/types/airline_type.rb
+++ app/graphql/types/airline_type.rb
@@ -4,6 +4,7 @@ module Types
     field :name, String, null: true
     field :image_url, String, null: true
     field :slug, String, null: true
+    field :average_score, Float, null: true, method: :average_score
     field :reviews, [ReviewType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: true
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
