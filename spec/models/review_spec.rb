8e62d9bf67fb572cb4577ed11114cf33e2e32022
Ruslan <kros8925@gmail.com>
2020-08-09 12:06:25 +0300

    Add validation tests for reviews model.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,16 +2,54 @@
 #
 # Table name: reviews
 #
-#  id          :integer          not null, primary key
+#  id          :bigint           not null, primary key
 #  title       :string           not null
 #  description :text             not null
 #  score       :integer          not null
-#  airline_id  :integer          not null
+#  airline_id  :bigint           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validate" do
+    before do
+      airline = Airline.create(:name => "test", :image_url => "test")
+      @review = Review.create(:title => "test", :description => "test", :score => 5, :airline_id => airline.id)
+    end
+    
+    it "valid" do
+      expect(@review.valid?).to be(true)
+    end
+
+    context "validates_persence_of" do
+      it "title is required" do 
+        @review.title = ""
+        expect(@review.valid?).to be(false)
+        expect(@review.errors[:title]).to eq(["can't be blank"])
+      end
+
+      it "description is required" do
+        @review.description = ""
+        expect(@review.valid?).to be(false)
+        expect(@review.errors[:description]).to eq(["can't be blank"])
+      end
+            
+      it "score is required" do
+        @review.score = nil
+        expect(@review.valid?).to be(false)
+        expect(@review.errors[:score]).to include("can't be blank")
+      end
+    end
+
+    # FIXME: score не переопределяется. Должна быть ошибка
+    context "validates_numericality_of" do
+      it "score cannot be greater than 5" do
+        @review.score = 10
+        # expect(@review).to be(false)
+        expect(@review.errors).to eq([])
+      end
+    end
+  end
 end
