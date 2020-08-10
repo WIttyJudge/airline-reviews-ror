cff67246c5c4bdf2f9f0e99884405ae41b47c3db
Ruslan <kros8925@gmail.com>
2020-08-10 15:08:10 +0300

    Formatted

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -32,59 +32,63 @@ RSpec.describe Airline, type: :model do
       expect(@airline.valid?).to be(false)
       expect(@airline.errors[:image_url]).to eq ["can't be blank"]
     end
-
-    context "before_create" do
-      it "name_to_title" do
-        airline = Airline.create(:name => "test:test TesT", :image_url => "https://test.com")
-        expect(airline.name).to eq("Test:Test Tes T")
-      end
-    end
-
-    context "validates_uniqueness_of" do
-      it "name must be unique" do
-        first_airline = Airline.create(:name => "test", :image_url => "https://test.com")
-        second_airline = Airline.create(:name => "test", :image_url => "https://test.com")
-
-        expect(second_airline.valid?).to be(false)
-        expect(second_airline.errors).to eq(["A company with this name already exists"])
-      end
-    end
-
-    context "class_methods" do 
-      context "average_score" do
-        it "airline average score is correct" do
-          airline = Airline.create(:name => "test", :image_url => "https://test.com")
-          reviews = Review.create([
-            {
-              :title => "test title",
-              :description => "first description",
-              :score => 2,
-              :airline_id => airline.id
-            },
-            {
-              :title => "test title",
-              :description => "first description",
-              :score => 5,
-              :airline_id => airline.id
-            },
-            {
-              :title => "test title",
-              :description => "first description",
-              :score => 1,
-              :airline_id => airline.id
-            },
-          ])
-
-          # (2+5+1) / 3
-          expect(airline.average_score).to eq(2.67)
-        end
-
-        it "if we dont have any reviews, return 0" do 
-          airline = Airline.create(:name => "test", :image_url => "https://test.com")
-          expect(airline.average_score).to eq(0)
-        end
-      end
-    end
-
   end
+
+  context "validates_uniqueness_of" do
+    it "name must be unique" do
+      first_airline = Airline.create(:name => "test", :image_url => "https://test.com")
+      second_airline = Airline.create(:name => "test", :image_url => "https://test.com")
+
+      expect(second_airline.valid?).to be(false)
+      expect(second_airline.errors[:name]).to eq(["A company with this name already exists"])
+    end
+  end
+
+  context "before_create" do
+    it "name_to_title" do
+      airline = Airline.create(:name => "test:test TesT", :image_url => "https://test.com")
+      expect(airline.name).to eq("Test:Test Tes T")
+    end
+
+    it "slugify" do
+      expect(true).to be(true)
+    end
+  end
+
+  context "class_methods" do 
+    context "average_score" do
+      it "the airline average score is correct" do
+        airline = Airline.create(:name => "test", :image_url => "https://test.com")
+        reviews = Review.create([
+          {
+            :title => "test title",
+            :description => "first description",
+            :score => 2,
+            :airline_id => airline.id
+          },
+          {
+            :title => "test title",
+            :description => "first description",
+            :score => 5,
+            :airline_id => airline.id
+          },
+          {
+            :title => "test title",
+            :description => "first description",
+            :score => 1,
+            :airline_id => airline.id
+          },
+        ])
+
+        # (2+5+1) / 3
+        expect(airline.average_score).to eq(2.67)
+      end
+
+      it "if we dont have any reviews, it will return 0" do 
+        airline = Airline.create(:name => "test", :image_url => "https://test.com")
+        expect(airline.average_score).to eq(0)
+      end
+    end
+  end
+
 end
