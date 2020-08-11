18aa34d7baf6d28a2a06536c5447ddb84e72ec7e
Ruslan <kros8925@gmail.com>
2020-08-11 20:35:52 +0300

    Modify mutation_type by adding namespace in field

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_review, mutation: Mutations::CreateReview
-
-    field :create_airline, mutation: Mutations::CreateAirline
+    field :create_review, mutation: Mutations::Review::CreateReview
+    # field :delete_review, mutation: Mutations::Review::DeleteReview
+    
+    field :create_airline, mutation: Mutations::Airline::CreateAirline
   end
 end
