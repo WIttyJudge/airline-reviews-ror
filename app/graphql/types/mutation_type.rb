8db2afaa2f54202c782d0b7f33d2046e6e0db0da
Ruslan <kros8925@gmail.com>
2020-08-17 15:26:04 +0300

    Delete unused code in mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_review, mutation: Mutations::Review::CreateReview
-    # field :delete_review, mutation: Mutations::Review::DeleteReview
     
     field :create_airline, mutation: Mutations::Airline::CreateAirline
   end
