b05e924f93ff2cecce40f166cf764626d12f2086
Ruslan <kros8925@gmail.com>
2020-08-30 12:08:28 +0300

    Create REST controller for an airline model.

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,16 +1,45 @@
-module Api 
+module Api
   module V1
     
     class AirlinesController < ApplicationController
       def index
-        airline = Airline.all
-        render json: airline
+        @airline = Airline.all
+        render json: @airline
       end
 
       def show
-        airline = Airline.find_by(slug: params[:slug])
-        if airline
-          render json: airline
+        @airline = find_airline
+        if @airline
+          render json: @airline
+        end
+      end
+
+      def create
+        @airline = Airline.create(airline_params)
+        if @airline.save
+          render json: @airline
+        else
+          render json: { error: 'Cannot create new airline' }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        @airline = find_airline
+        if @airline
+          @airline.update(airline_params)
+          render json: { message: 'Airline successfully updated' }, status: :ok
+        else
+          render json: { error: 'Cannot update airline' }
+        end
+      end
+
+      def destroy
+        @airline = find_airline
+        if @airline
+          @airline.destroy
+          render json: { message: 'Airline successfully deleted' }, status: :ok
+        else
+          render json: { error: 'Cannot delete airline' }, status: :no_content
         end
       end
 
@@ -19,6 +48,11 @@ module Api
       def airline_params
         params.require(:airline).permit(:name, :image_url)
       end
+
+      def find_airline
+        Airline.find_by(slug: params[:slug])
+      end
     end
+
   end
 end
